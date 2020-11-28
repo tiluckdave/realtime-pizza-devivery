132ee31ca53ee51b63d423de3b33bf1ff2261c21
Tilak <davetilak003@gmail.com>
2020-11-28 17:16:15 +0530

    Created add to cart functionality

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+
+// Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('database Connected...');
+}).catch(err => {
+    console.log('connection failed...');
+});
+
+
+// Session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+// Session Config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}))
+
+app.use(flash())
 
 // Assets
 app.use(express.static('public'))
+app.use(express.json())
 
+
+// Global middlrware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 // Set Template  engine
 app.use(expressLayout)
@@ -15,21 +55,7 @@ app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
+require('./routes/web')(app)
 
 
 app.listen(PORT, () => {
